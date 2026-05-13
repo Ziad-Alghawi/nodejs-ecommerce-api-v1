f66472a33369e825ac851ccb4e9371f3e6b107d5
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-05-13 21:01:08 +0200

    feat: add error handling middleware and ApiError class for better error management

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import dbConnection from "./config/database.js";
 import categoryRoute from "./routes/categoryRoute.js";
+import ApiError from "./utils/apiError.js";
 
 dotenv.config({ path: "config.env" });
 
@@ -22,8 +23,28 @@ if (process.env.NODE_ENV === "development") {
 // mount Routes
 app.use("/api/v1/categories", categoryRoute);
 
-const PORT = process.env.PORT || 8000;
+// Handle unhandled routes
+app.all("/*splat", (req, res, next) => {
+  // Create error and send it to error handling middleware
+  //const err = new Error(`Can't find this route: ${req.originalUrl}`);
+  //next(err);
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
+});
 
+// Global error handler middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
