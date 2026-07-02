76e749519b31fb1212a890ddbf9dfa272952172c
Ziad Alghawi <ziad97algawy@gmail.com>
2026-07-02 21:21:17 +0200

    feat: add brand image upload and refactor uploadImageMiddleware

--- routes/brandRoute.js
+++ routes/brandRoute.js
@@ -12,16 +12,20 @@ import {
   getBrand,
   updateBrand,
   deleteBrand,
+  uploadBrandImage,
+  resizeImage,
 } from "../services/brandService.js";
 
 const router = express.Router();
 
 // http://localhost:8000/api/v1/brands
-router.route("/").get(getBrands).post(createBrandValidator, createBrand);
+router.route("/").get(getBrands).post(uploadBrandImage,
+  resizeImage, createBrandValidator, createBrand);
 router
   .route("/:id")
   .get(getBrandValidator, getBrand)
-  .put(updateBrandValidator, updateBrand)
+  .put(uploadBrandImage,
+  resizeImage, updateBrandValidator, updateBrand)
   .delete(deleteBrandValidator, deleteBrand);
 
 export default router;
--- services/brandService.js
+++ services/brandService.js
@@ -1,6 +1,32 @@
+import sharp from 'sharp';
+import asyncHandler from "express-async-handler";
+import { v4 as uuidv4 } from "uuid";
+
 import * as factory from "./handlersFactory.js";
+import { uploadSingleImage} from "../middlewares/uploadImageMiddleware.js";
+
 
 import Brand from "../models/brandModel.js";
+
+
+// uplosad single image
+export const uploadBrandImage = uploadSingleImage("image");
+
+// image processing
+export const resizeImage = asyncHandler(async (req, res, next) => {
+  const filename = `brand-${uuidv4()}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+  .resize(800, 800)
+  .toFormat("jpeg")
+  .jpeg({quality: 95})
+  .toFile(`uploads/brands/${filename}`);
+
+  // save image into our db
+  req.body.image = filename;
+
+  next();
+});
 
 // @desc Get list of brands
 // @route GET /api/v1/brands
